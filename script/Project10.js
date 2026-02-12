299a45d1d57ae6589a1ba0367e02ead887276e90
Maxjell <maxwellkwameagyeman@gmail.com>
2026-02-11 21:23:07 -0800

    I have added arrays

--- script/Project10.js
+++ script/Project10.js
@@ -3,13 +3,13 @@ let level = 10 ;
 let bonus = true ;
 
 if(bonus){
-    score = score + 40
+    score += 40;
     if(level > 5){
-        score = score + 20
+        score += 20;
     }
 }
 if(score>=100){
-    console.log(`You win`)
+    console.log(`You win`);
 }else{
-    console.log(`Better luck next time , your score is ${score}`)
+    console.log(`Better luck next time , your score is ${score}`);
 }
---
+++ script/array.js
@@ -0,0 +1,5 @@
+let students = ["Arnie","Blye","Karna"];
+students.push("Aly");
+students.shift(students);
+console.log(students);
+console.log(students.length);
