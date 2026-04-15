b53f95cb934a26e21dbc771e666987ab03dd6080
Maxjell <maxwellkwameagyeman@gmail.com>
2026-04-15 14:42:00 -0700

    i added a two number calculator

--- script/calc.js
+++ script/calc.js
@@ -2,29 +2,27 @@ let subtract = document.querySelector("#subtract");
 let add = document.getElementById("add");
 let multiply = document.querySelector("#multiply");
 let divide = document.querySelector("#divide");
-let numOne = document.querySelector("#numOne");
-let numTwo = document.querySelector("#numTwo");
 let output = document.querySelector("#output");
 
 function subtraction(){
     let numOne = Number(document.querySelector("#numOne").value);
-    let numTwo = Number(document.querySelector("#numOne").value);
+    let numTwo = Number(document.querySelector("#numTwo").value);
     output.innerText = numOne - numTwo;
 }
 function addition(){
     let numOne = Number(document.querySelector("#numOne").value);
-    let numTwo = Number(document.querySelector("#numOne").value);
+    let numTwo = Number(document.querySelector("#numTwo").value);
     output.innerText = numOne + numTwo;
 }
 function multiplication(){
     let numOne = Number(document.querySelector("#numOne").value);
-    let numTwo = Number(document.querySelector("#numOne").value);
+    let numTwo = Number(document.querySelector("#numTwo").value);
     output.innerText = numOne * numTwo;
 }
 function division(){
     let numOne = Number(document.querySelector("#numOne").value);
-    let numTwo = Number(document.querySelector("#numOne").value);
-    output.innerText = numOne / numTwo;
+    let numTwo = Number(document.querySelector("#numTwo").value);
+    output.innerText = numTwo !== 0 ? numOne / numTwo : "Cannot divide by zero";
 }
 
 subtract.onclick = subtraction;
